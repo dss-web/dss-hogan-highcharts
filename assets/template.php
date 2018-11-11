d5a5562043a1af43a259a87307a32f93c0064d08
soderlind <per@soderlind.no>
2018-11-11 14:45:01 +0100

    housekeeping

--- assets/template.php
+++ assets/template.php
@@ -15,28 +15,6 @@ namespace Dekode\Hogan;
 if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof DSS_Highcharts ) ) {
 	return; // Exit if accessed directly.
 }
-
-if ( ! function_exists( 'write_log' ) ) {
-	/**
-	* Utility function for logging arbitrary variables to the error log.
-	*
-	* Set the constant WP_DEBUG to true and the constant WP_DEBUG_LOG to true to log to wp-content/debug.log.
-	* You can view the log in realtime in your terminal by executing `tail -f debug.log` and Ctrl+C to stop.
-	*
-	* @param mixed $log Whatever to log.
-	*/
-	function write_log( $log ) {
-		if ( true === WP_DEBUG ) {
-			if ( is_scalar( $log ) ) {
-				error_log( $log );
-			} else {
-				error_log( print_r( $log, true ) );
-			}
-		}
-	}
-}
-
-
 ?>
 
 <ul class="list-items card-type-large">
@@ -46,25 +24,12 @@ if ( ! function_exists( 'write_log' ) ) {
 	foreach ( $this->highcharts as $graf ) :
 		?>
 			<?php
-			write_log( $graf );
 			$title     = $graf['title'];
 			$file_name = $graf['file']['url'];
-			// $attachment_file_size           = filesize( get_attached_file( $graf['file']['id'] ) );
-			// $attachment_file_size_formatted = size_format( $attachment_file_size );
-			// $max_chars                      = 50;
-			// if ( strlen( $file_name ) > $max_chars ) {
-			// 	$mime_type_array   = explode( '/', $graf['file']['mime_type'] );
-			// 	$mime_type         = $mime_type_array[ count( $mime_type_array ) - 1 ];
-			// 	$file_name_chopped = substr( $file_name, 0, ( $max_chars - 3 ) ) . '...' . $mime_type;
-			// } else {
-			// 	$file_name_chopped = $file_name;
-			// }
-			$float = '';
-			// printf('<div id="container-%s" class="graph %s">graf</div>',$counter, $float);
 			printf(
 				'<li class="list-item">
 						<div  class="column">
-							<div id="container-%s" class="featured-image graph">
+							<div id="container-%s" class="graph">
 								<span >graf</span>
 							</div>
 						</div>
@@ -76,69 +41,43 @@ if ( ! function_exists( 'write_log' ) ) {
 			printf( '<script>' );
 			printf(
 				"jQuery.get('%s', function(csvFile) {
-			jQuery('#container-%s').highcharts({
-				chart: {
-					type: 'column',
-					backgroundColor: '#f1f1f1',
-					style: {
-							fontFamily: 'Open Sans',
-							fontSize: '12px'
-					},
-					width: 500,
-					height: 500
-				},
-				data: {
-					csv: csvFile
-				},
-
-				//colors: ['#7E287D', '#655E27', '#E0D020', '#FF71AB','#801c7d'],
-				colors: ['#7E287D', '#E0D020','#655E27', '#FF71AB','#801c7d'],
-				title: {
-					text: '%s'
-				},
-				// legend: {
-				// 	align: 'right',
-				// 	verticalAlign: 'middle',
-				// 	layout: 'vertical'
-				// },
-				yAxis: {
-					title: {
-						text: ''
-					},
-				},
-				responsive: {
-					rules: [{
-						condition: {
-							maxWidth: 500,
-							maxHeight: 500
+					jQuery('#container-%s').highcharts({
+						chart: {
+							type: 'column',
+							backgroundColor: '#f1f1f1',
+							style: {
+									fontFamily: 'Open Sans',
+									fontSize: '12px'
+							},
 						},
-						chartOptions: {
-							// legend: {
-							// 	align: 'center',
-							// 	verticalAlign: 'bottom',
-							// 	layout: 'horizontal'
-							// },
-							yAxis: {
-								labels: {
-									align: 'left',
-									x: 0,
-									y: -5
+						data: {
+							csv: csvFile
+						},
+
+						//colors: ['#7E287D', '#655E27', '#E0D020', '#FF71AB','#801c7d'],
+						colors: ['#7E287D', '#E0D020','#655E27', '#FF71AB','#801c7d'],
+						title: {
+							text: '%s'
+						},
+						yAxis: {
+							title: {
+								text: ''
+							},
+						},
+						responsive: {
+							rules: [{
+								condition: {
+									maxWidth: 400
 								},
-								title: {
-									text: null
+								chartOptions: {
+									chart: {
+										className: 'small-chart'
+									}
 								}
-							},
-							subtitle: {
-								text: null
-							},
-							credits: {
-								enabled: false
-							}
+							}]
 						}
-					}]
-				}
-			});
-		});",
+					});
+				});",
 				$file_name,
 				$counter,
 				$title
--- dss-hogan-highcharts.php
+++ dss-hogan-highcharts.php
@@ -92,22 +92,3 @@ function on_hogan_inner_wrapper_classes( $classes, $module ) {
 	return $classes;
 }
 
-if ( ! function_exists( 'write_log' ) ) {
-	/**
-	* Utility function for logging arbitrary variables to the error log.
-	*
-	* Set the constant WP_DEBUG to true and the constant WP_DEBUG_LOG to true to log to wp-content/debug.log.
-	* You can view the log in realtime in your terminal by executing `tail -f debug.log` and Ctrl+C to stop.
-	*
-	* @param mixed $log Whatever to log.
-	*/
-	function write_log( $log ) {
-		if ( true === WP_DEBUG ) {
-			if ( is_scalar( $log ) ) {
-				error_log( $log );
-			} else {
-				error_log( print_r( $log, true ) );
-			}
-		}
-	}
-}
