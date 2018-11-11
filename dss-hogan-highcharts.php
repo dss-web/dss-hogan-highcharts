710aef40e6d4e51b28e483e0165837987cdd42e3
soderlind <per@soderlind.no>
2018-11-11 14:21:19 +0100

    Update plugin header

--- dss-hogan-highcharts.php
+++ dss-hogan-highcharts.php
@@ -1,21 +1,25 @@
 <?php
 /**
+ * DSS Hogan Module: Highcharts
+ *
+ * @package     hogan
+ * @author      Per Soderlind
+ * @copyright   2018 Per Soderlind
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
  * Plugin Name: DSS Hogan Module: Highcharts
- * Plugin URI: https://github.com/dss-web/dss-hogan-highcharts
- * GitHub Plugin URI: https://github.com/dss-web/dss-hogan-highcharts
+ * Plugin URI: https://github.com/soderlind/dss-hogan-highcharts
+ * GitHub Plugin URI: https://github.com/soderlind/dss-hogan-highcharts
  * Description: DSS highcharts Module for Hogan.
- * Version: 1.0.1
- * Author: Dekode
- * Author URI: https://dekode.no
- * License: GPL-3.0
- * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
- *
+ * Version:     0.0.1
+ * Author:      Per Soderlind
+ * Author URI:  https://soderlind.no
  * Text Domain: dss-hogan-highcharts
- * Domain Path: /languages/
- *
- * @package Hogan
- * @author Dekode
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
 
 declare( strict_types = 1 );
 
